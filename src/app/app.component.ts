b1f22140fa1fcdd7e22a540df421e24733aab5bf
sreeram <mlnsreeram@gmail.com>
2021-04-06 15:12:41 +0530

    password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordgenerator';
+  password = '';
+  includeletters = false;
+  includenumbers = false;
+  includesymbols = false;
+  passwordlength = 0;
+  onButtonClick() {
+    let generatedpwd = '';
+    let validchars = '';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmn';
+    const symbols = '!@#$%^&*()';
+    if (this.includeletters) {
+      validchars += letters;
+    }
+    if (this.includenumbers) {
+      validchars += numbers;
+    }
+    if (this.includesymbols) {
+      validchars += symbols;
+    }
+    for (let i = 0; i < this.passwordlength; i++) {
+      const index = Math.floor(Math.random() * validchars.length);
+      generatedpwd += validchars[index];
+    }
+    this.password = generatedpwd;
+  }
+  useletters() {
+    this.includeletters = !this.includeletters;
+  }
+  usenumbers() {
+    this.includenumbers = !this.includenumbers;
+  }
+  usesymbols() {
+    this.includesymbols = !this.includesymbols;
+  }
+  onchangelength(value : string) {
+    const parsedval =  parseInt(value);
+    if(!isNaN(parsedval)){
+      this.passwordlength = parsedval;
+    }
+    else{
+      alert('please enter number only')
+    }
+  }
 }
